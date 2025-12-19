1bf0bbf03db6cdeaed5c00b07364b4bcf9711400
Carl Rham Jhay <dayaocarlrhamjhay@gmail.com>
2025-12-19 16:39:56 +0800

    I will commiting these file

--- JS/Dayao.js
+++ JS/Dayao.js
@@ -30,12 +30,46 @@ document.querySelectorAll(".scroll-down").forEach(button => {
   });
 });
 
-// Scroll Up button (Contact → Home)
+
 document.querySelectorAll(".scroll-up").forEach(button => {
   button.addEventListener("click", () => {
-    const target = document.getElementById("home"); // force scroll to top
+    const target = document.getElementById("home");
     if (target) {
       target.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
+
+const quote = document.getElementById("quote");
+
+const getquote = async () => {
+  try {
+    const res = await fetch(
+      `https://api.adviceslip.com/advice`
+    );
+const data = await res.json();
+quote.innerHTML = `
+ <div style="
+    margin-bottom:12px;
+    margin-top:12px;
+    padding:12px;
+    background:rgba(10, 20, 60, 0.4);
+    backdrop-filter:blur(6px);
+    border:1px solid rgba(30, 50, 120, 0.5);
+    border-radius:10px;
+    color:#dce6ff;
+    font-size:14px;
+    line-height:1.3;
+    max-width:300px;
+  ">
+    <p style="margin:4px 0; font-style:italic; color:#bcd0ff;">${data.slip.advice}</p>`;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getquote();
+
+
+
